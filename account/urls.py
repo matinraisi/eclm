8ccb19e276f483e5feba3eb85048d914774f9636
matinraisi <raisi.m630@gmail.com>
2025-07-26 18:57:06 +0330

    account login & register & logout view

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .views import *
 app_name = 'account'
 urlpatterns = [
-    path('register/' , RegisterVeiw.as_view() , name='register')
+    path('register/' , RegisterVeiw.as_view() , name='register'),
+    path('login/' , LoginView.as_view() , name='login'),
+    path('logout/' , Logout.as_view() , name='Logout'),
+
 ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render , redirect
 from django.views.generic import View
 from django.contrib.auth import authenticate,login,logout
-from .models import *
+from .models import CustomUser
+
 # Create your views here.
 
 class RegisterVeiw(View):
-    
+    template_name = 'core/index.html'
 
     def post( self, request):
         email = request.POST.get("email")
@@ -13,11 +14,46 @@ class RegisterVeiw(View):
         password1 = request.POST.get("password1")
         password2 = request.POST.get("password2")
 
-        print(email,phone,password1,password2)
+        if not email or not phone or not password1 or not password2:
+            return render(request , self.template_name, {'error' : "فیلد هارو کامل وارد کنید"})
 
+        if CustomUser.objects.filter(email=email).exists() :
+            return render(request , self.template_name, {'error' : "ایمیل شما تکراری است و وجود دارد"})
+        if password1 != password2 :
+            return render(request , self.template_name, {'error' : "رمز عبور با تکرار رمز عبور مغایرت ندارد"})
+
+        # ولیدشن شماره تلفن  
+        # هش کردن پسورد
+        user = CustomUser.objects.create(
+                username = email,
+                email = email,
+                phone_number = phone,
+                password = password1,
+        )
+
+        login(request , user)
 
         return redirect("core:home")
         
+
+class LoginView(View):
+    template_name = 'core/index.html'
+    def post(self , request):
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request ,email=email , password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("core:home")
+        else:
+            
+            return render(request, self.template_name, {"error": "ایمیل یا رمز عبور اشتباه است."})
+
+
+class Logout(View):
+    def get(self , request):
+        logout(request)
+        return redirect("core:home")
 
 
 
